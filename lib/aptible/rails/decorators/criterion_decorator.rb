4dff6a6da0bffce59cdb96f8e53f13d449789008
Frank Macreery <frank@macreery.com>
2014-07-23 21:59:37 -0700

    Better decorator methods around criteria status

--- lib/aptible/rails/decorators/criterion_decorator.rb
+++ lib/aptible/rails/decorators/criterion_decorator.rb
@@ -17,12 +17,31 @@ class CriterionDecorator < ApplicationDecorator
     self.class.status_description(object.status)
   end
 
+  # rubocop:disable MethodLength
   def update_summary
-    if current_document
-      "Updated #{h.time_ago_in_words(current_document.created_at)} ago"
-    else
-      'Never updated'
+    case object.scope
+    when 'organization'
+      if current_document
+        "Updated #{h.time_ago_in_words(current_document.created_at)} ago"
+      else
+        'Never updated'
+      end
+    when 'app'
+      count = unique_app_count
+      "Completed for #{count} #{'app'.pluralize(count)}"
+    when 'user'
+      count = unique_user_count
+      "Completed for #{count} #{'user'.pluralize(count)}"
     end
+  end
+  # rubocop:enable MethodLength
+
+  def unique_user_count
+    object.documents.map { |d| d.links['user'].href }.uniq.count
+  end
+
+  def unique_app_count
+    object.documents.map { |d| d.links['app'].href }.uniq.count
   end
 
   def current_document
--- lib/aptible/rails/version.rb
+++ lib/aptible/rails/version.rb
@@ -1,5 +1,5 @@
 module Aptible
   module Rails
-    VERSION = '0.4.5'
+    VERSION = '0.4.6'
   end
 end
