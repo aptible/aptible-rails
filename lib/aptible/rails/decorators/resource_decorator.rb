d25d169da158bfece58053e29ab0cce310be69a4
Skylar Anderson <Skylar@Anderson.is>
2014-08-15 08:46:39 -0400

    cleanup

--- lib/aptible/rails/decorators/resource_decorator.rb
+++ lib/aptible/rails/decorators/resource_decorator.rb
@@ -2,9 +2,7 @@
 class ResourceDecorator < ApplicationDecorator
   def last_operation_gravatar
     garner.bind(h.controller.session_token).bind(object) do
-      unless last_operation.user.nil?
-        h.gravatar_url(last_operation.user.email, 32)
-      end
+      h.gravatar_url(last_operation.user.email, 32)
     end
   end
 
